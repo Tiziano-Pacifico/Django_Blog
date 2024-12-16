31cbac3f8341dcc060744b98f4e06d613803b693
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-12-16 21:21:29 +0100

    single blog page

--- blogs/views.py
+++ blogs/views.py
@@ -19,3 +19,16 @@ def posts_by_category(request, category_id):
         'category': category,
     }
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    if settings.DEBUG:
+        try:
+            single_blog = Blog.objects.get(slug=slug)
+        except:
+            return render(request, '404.html')
+    else:
+        single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    context ={
+        'single_blog': single_blog,
+    }
+    return render(request, 'blogs.html', context)
