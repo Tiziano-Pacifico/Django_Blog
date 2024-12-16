dfc93d2083929a9d8aaba05bc17b28b84d7ffbcb
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-12-16 22:10:46 +0100

    assignements: side elements

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from assignements.models import About
 from blogs.models import Blog, Category
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured = True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
+    try:
+        about = About.objects.get()
+    except:
+        about = None
     context = {
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
 
