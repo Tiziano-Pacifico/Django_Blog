265b488b493d73d073c98b8d2cd5b51d9820aba2
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-12-17 21:54:55 +0100

    search functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/',admin.site.urls),
     path('category/', include('blogs.urls')),
     path('<slug:slug>/', BlogsView.blogs, name='blogs'),
+    path('blog/search/', BlogsView.search, name='search')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
