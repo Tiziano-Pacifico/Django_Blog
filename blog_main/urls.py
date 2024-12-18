920d5c0740d8ac3a80a002c300ec2468fa71c671
Tiziano Pacifico <gourmetmolise@gmail.com>
2024-12-18 22:59:44 +0100

    register login logout

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,8 +25,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin/',admin.site.urls),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>/', BlogsView.blogs, name='blogs'),
-    path('blog/search/', BlogsView.search, name='search')
+    path('blog/<slug:slug>/', BlogsView.blogs, name='blogs'),
+    path('blog/search/', BlogsView.search, name='search'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from assignements.models import About
 from blogs.models import Blog, Category
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured = True, status='Published').order_by('updated_at')
@@ -16,5 +19,43 @@ def home(request):
         'about': about,
     }
     return render(request, 'home.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        #La request è necessaria per altre funzionalità, come per esempio verificare s l'utente è gia loggato
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            #prende nome utente e password e restituisce l'utente se valido
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
 
 
